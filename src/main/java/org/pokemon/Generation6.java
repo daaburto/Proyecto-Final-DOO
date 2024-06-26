a48426c1f7140b671cd9e364919c5bd5c7cdf45b
Daniel <166765631+daaburto@users.noreply.github.com>
2024-06-26 19:02:39 -0400

    Pokemon de 6ta, 7ma y 8va generacion añadidos (con megaevoluciones y formas regionales)

---
+++ src/main/java/org/pokemon/Generation6.java
@@ -0,0 +1,179 @@
+package org.pokemon;
+
+import javax.swing.*;
+import java.awt.*;
+
+public enum Generation6 {
+    CHESPIN("Chespin", "650", "src/main/resources/pokemon/650.png", 56, Type.GRASS),
+    QUILLADIN("Quilladin", "651", "src/main/resources/pokemon/651.png", 61, Type.GRASS),
+    CHESNAUGHT("Chesnaught", "652", "src/main/resources/pokemon/652.png", 88, Type.GRASS, Type.FIGHTING),
+    FENNEKIN("Fennekin", "653", "src/main/resources/pokemon/653.png", 40, Type.FIRE),
+    BRAIXEN("Braixen", "654", "src/main/resources/pokemon/654.png", 59, Type.FIRE),
+    DELPHOX("Delphox", "655", "src/main/resources/pokemon/655.png", 75, Type.FIRE, Type.PSYCHIC),
+    FROAKIE("Froakie", "656", "src/main/resources/pokemon/656.png", 41, Type.WATER),
+    FROGADIER("Frogadier", "657", "src/main/resources/pokemon/657.png", 54, Type.WATER),
+    GRENINJA("Greninja", "658", "src/main/resources/pokemon/658.png", 72, Type.WATER, Type.DARK),
+    BUNNELBY("Bunnelby", "659", "src/main/resources/pokemon/659.png", 38, Type.NORMAL),
+    DIGGERSBY("Diggersby", "660", "src/main/resources/pokemon/660.png", 85, Type.NORMAL, Type.GROUND),
+    FLETCHLING("Fletchling", "661", "src/main/resources/pokemon/661.png", 45, Type.NORMAL, Type.FLYING),
+    FLETCHINDER("Fletchinder", "662", "src/main/resources/pokemon/662.png", 62, Type.FIRE, Type.FLYING),
+    TALONFLAME("Talonflame", "663", "src/main/resources/pokemon/663.png", 78, Type.FIRE, Type.FLYING),
+    SCATTERBUG("Scatterbug", "664", "src/main/resources/pokemon/664.png", 38, Type.BUG),
+    SPEWPA("Spewpa", "665", "src/main/resources/pokemon/665.png", 45, Type.BUG),
+    VIVILLON("Vivillon", "666", "src/main/resources/pokemon/666.png", 80, Type.BUG, Type.FLYING),
+    LITLEO("Litleo", "667", "src/main/resources/pokemon/667.png", 62, Type.FIRE, Type.NORMAL),
+    PYROAR("Pyroar", "668", "src/main/resources/pokemon/668.png", 86, Type.FIRE, Type.NORMAL),
+    FLABEBE("Flabébé", "669", "src/main/resources/pokemon/669.png", 44, Type.FAIRY),
+    FLOETTE("Floette", "670", "src/main/resources/pokemon/670.png", 54, Type.FAIRY),
+    FLORGES("Florges", "671", "src/main/resources/pokemon/671.png", 78, Type.FAIRY),
+    SKIDDO("Skiddo", "672", "src/main/resources/pokemon/672.png", 66, Type.GRASS),
+    GOGOAT("Gogoat", "673", "src/main/resources/pokemon/673.png", 123, Type.GRASS),
+    PANCHAM("Pancham", "674", "src/main/resources/pokemon/674.png", 67, Type.FIGHTING),
+    PANGORO("Pangoro", "675", "src/main/resources/pokemon/675.png", 95, Type.FIGHTING, Type.DARK),
+    FURFROU("Furfrou", "676", "src/main/resources/pokemon/676.png", 75, Type.NORMAL),
+    ESPURR("Espurr", "677", "src/main/resources/pokemon/677.png", 62, Type.PSYCHIC),
+    MEOWSTIC_MALE("Meowstic (Male)", "678", "src/main/resources/pokemon/678-male.png", 74, Type.PSYCHIC),
+    MEOWSTIC_FEMALE("Meowstic (Female)", "678", "src/main/resources/pokemon/678-female.png", 74, Type.PSYCHIC),
+    HONEDGE("Honedge", "679", "src/main/resources/pokemon/679.png", 45, Type.STEEL, Type.GHOST),
+    DOUBLADE("Doublade", "680", "src/main/resources/pokemon/680.png", 59, Type.STEEL, Type.GHOST),
+    AEGISLASH("Aegislash", "681", "src/main/resources/pokemon/681.png", 60, Type.STEEL, Type.GHOST),
+    SPRITZEE("Spritzee", "682", "src/main/resources/pokemon/682.png", 78, Type.FAIRY),
+    AROMATISSE("Aromatisse", "683", "src/main/resources/pokemon/683.png", 101, Type.FAIRY),
+    SWIRLIX("Swirlix", "684", "src/main/resources/pokemon/684.png", 62, Type.FAIRY),
+    SLURPUFF("Slurpuff", "685", "src/main/resources/pokemon/685.png", 82, Type.FAIRY),
+    INKAY("Inkay", "686", "src/main/resources/pokemon/686.png", 53, Type.DARK, Type.PSYCHIC),
+    MALAMAR("Malamar", "687", "src/main/resources/pokemon/687.png", 86, Type.DARK, Type.PSYCHIC),
+    BINACLE("Binacle", "688", "src/main/resources/pokemon/688.png", 42, Type.ROCK, Type.WATER),
+    BARBARACLE("Barbaracle", "689", "src/main/resources/pokemon/689.png", 72, Type.ROCK, Type.WATER),
+    SKRELP("Skrelp", "690", "src/main/resources/pokemon/690.png", 50, Type.POISON, Type.WATER),
+    DRAGALGE("Dragalge", "691", "src/main/resources/pokemon/691.png", 65, Type.POISON, Type.DRAGON),
+    CLAUNCHER("Clauncher", "692", "src/main/resources/pokemon/692.png", 50, Type.WATER),
+    CLAWITZER("Clawitzer", "693", "src/main/resources/pokemon/693.png", 71, Type.WATER),
+    HELIOPTILE("Helioptile", "694", "src/main/resources/pokemon/694.png", 44, Type.ELECTRIC, Type.NORMAL),
+    HELIOLISK("Heliolisk", "695", "src/main/resources/pokemon/695.png", 62, Type.ELECTRIC, Type.NORMAL),
+    TYRUNT("Tyrunt", "696", "src/main/resources/pokemon/696.png", 58, Type.ROCK, Type.DRAGON),
+    TYRANTRUM("Tyrantrum", "697", "src/main/resources/pokemon/697.png", 82, Type.ROCK, Type.DRAGON),
+    AMAURA("Amaura", "698", "src/main/resources/pokemon/698.png", 77, Type.ROCK, Type.ICE),
+    AURORUS("Aurorus", "699", "src/main/resources/pokemon/699.png", 123, Type.ROCK, Type.ICE),
+    SYLVEON("Sylveon", "700", "src/main/resources/pokemon/700.png", 95, Type.FAIRY),
+    HAWLUCHA("Hawlucha", "701", "src/main/resources/pokemon/701.png", 78, Type.FIGHTING, Type.FLYING),
+    DEDENNE("Dedenne", "702", "src/main/resources/pokemon/702.png", 67, Type.ELECTRIC, Type.FAIRY),
+    CARBINK("Carbink", "703", "src/main/resources/pokemon/703.png", 50, Type.ROCK, Type.FAIRY),
+    GOOMY("Goomy", "704", "src/main/resources/pokemon/704.png", 45, Type.DRAGON),
+    SLIGGOO("Sliggoo", "705", "src/main/resources/pokemon/705.png", 68, Type.DRAGON),
+    GOODRA("Goodra", "706", "src/main/resources/pokemon/706.png", 90, Type.DRAGON),
+    KLEFKI("Klefki", "707", "src/main/resources/pokemon/707.png", 57, Type.STEEL, Type.FAIRY),
+    PHANTUMP("Phantump", "708", "src/main/resources/pokemon/708.png", 43, Type.GHOST, Type.GRASS),
+    TREVENANT("Trevenant", "709", "src/main/resources/pokemon/709.png", 85, Type.GHOST, Type.GRASS),
+    PUMPKABOO("Pumpkaboo", "710", "src/main/resources/pokemon/710.png", 49, Type.GHOST, Type.GRASS),
+    GOURGEIST("Gourgeist", "711", "src/main/resources/pokemon/711.png", 65, Type.GHOST, Type.GRASS),
+    BERGMITE("Bergmite", "712", "src/main/resources/pokemon/712.png", 55, Type.ICE),
+    AVALUGG("Avalugg", "713", "src/main/resources/pokemon/713.png", 95, Type.ICE),
+    NOIBAT("Noibat", "714", "src/main/resources/pokemon/714.png", 40, Type.FLYING, Type.DRAGON),
+    NOIVERN("Noivern", "715", "src/main/resources/pokemon/715.png", 85, Type.FLYING, Type.DRAGON),
+    XERNEAS("Xerneas", "716", "src/main/resources/pokemon/716.png", 126, Type.FAIRY),
+    YVELTAL("Yveltal", "717", "src/main/resources/pokemon/717.png", 126, Type.DARK, Type.FLYING),
+    ZYGARDE_50("Zygarde (50% Forme)", "718", "src/main/resources/pokemon/718.png", 108, Type.DRAGON, Type.GROUND),
+    DIANCIE("Diancie", "719", "src/main/resources/pokemon/719.png", 50, Type.ROCK, Type.FAIRY),
+    HOOPA("Hoopa (Confined)", "720", "src/main/resources/pokemon/720.png", 80, Type.PSYCHIC, Type.GHOST),
+    VOLCANION("Volcanion", "721", "src/main/resources/pokemon/721.png", 80, Type.FIRE, Type.WATER),
+
+    MEGA_VENUSAUR("Mega Venusaur", "001", "src/main/resources/pokemon/10033.png", 80, Type.GRASS, Type.POISON),
+    MEGA_CHARIZARD_X("Mega Charizard X", "006", "src/main/resources/pokemon/10034.png", 78, Type.FIRE, Type.DRAGON),
+    MEGA_CHARIZARD_Y("Mega Charizard Y", "006", "src/main/resources/pokemon/10035.png", 78, Type.FIRE, Type.FLYING),
+    MEGA_BLASTOISE("Mega Blastoise", "009", "src/main/resources/pokemon/10036.png", 79, Type.WATER),
+    MEGA_ALAKAZAM("Mega Alakazam", "065", "src/main/resources/pokemon/10037.png", 55, Type.PSYCHIC),
+    MEGA_GENGAR("Mega Gengar", "094", "src/main/resources/pokemon/10038.png", 60, Type.GHOST, Type.POISON),
+    MEGA_KANGASKHAN("Mega Kangaskhan", "115", "src/main/resources/pokemon/10039.png", 105, Type.NORMAL),
+    MEGA_PINSIR("Mega Pinsir", "127", "src/main/resources/pokemon/10040.png", 65, Type.BUG),
+    MEGA_GYARADOS("Mega Gyarados", "130", "src/main/resources/pokemon/10041.png", 95, Type.WATER, Type.DARK),
+    MEGA_AERODACTYL("Mega Aerodactyl", "142", "src/main/resources/pokemon/10042.png", 80, Type.ROCK, Type.FLYING),
+    MEGA_MEWTWO_X("Mega Mewtwo X", "150", "src/main/resources/pokemon/10043.png", 106, Type.PSYCHIC,Type.FIGHTING),
+    MEGA_MEWTWO_Y("Mega Mewtwo Y", "150", "src/main/resources/pokemon/10044.png", 106, Type.PSYCHIC),
+    MEGA_AMPHAROS("Mega Ampharos", "181", "src/main/resources/pokemon/10045.png", 90, Type.ELECTRIC, Type.DRAGON),
+    MEGA_SCIZOR("Mega Scizor", "212", "src/main/resources/pokemon/10046.png", 70, Type.BUG, Type.STEEL),
+    MEGA_HERACROSS("Mega Heracross", "214", "src/main/resources/pokemon/10047.png", 80, Type.BUG, Type.FIGHTING),
+    MEGA_HOUNDOOM("Mega Houndoom", "229", "src/main/resources/pokemon/10048.png", 75, Type.DARK, Type.FIRE),
+    MEGA_TYRANITAR("Mega Tyranitar", "248", "src/main/resources/pokemon/10049.png", 100, Type.ROCK, Type.DARK),
+    MEGA_BLAZIKEN("Mega Blaziken", "257", "src/main/resources/pokemon/10050.png", 80, Type.FIRE, Type.FIGHTING),
+    MEGA_GARDEVOIR("Mega Gardevoir", "282", "src/main/resources/pokemon/10051.png", 68, Type.PSYCHIC, Type.FAIRY),
+    MEGA_MAWILE("Mega Mawile", "303", "src/main/resources/pokemon/10052.png", 50, Type.STEEL, Type.FAIRY),
+    MEGA_AGGRON("Mega Aggron", "306", "src/main/resources/pokemon/10053.png", 70, Type.STEEL, Type.ROCK),
+    MEGA_MEDICHAM("Mega Medicham", "308", "src/main/resources/pokemon/10054.png", 60, Type.FIGHTING, Type.PSYCHIC),
+    MEGA_MANECTRIC("Mega Manectric", "310", "src/main/resources/pokemon/10055.png", 70, Type.ELECTRIC),
+    MEGA_BANETTE("Mega Banette", "354", "src/main/resources/pokemon/10056.png", 64, Type.GHOST),
+    MEGA_ABSOL("Mega Absol", "359", "src/main/resources/pokemon/10057.png", 65, Type.DARK),
+    MEGA_GARCHOMP("Mega Garchomp", "445", "src/main/resources/pokemon/10058.png", 108, Type.DRAGON, Type.GROUND),
+    MEGA_LUCARIO("Mega Lucario", "448", "src/main/resources/pokemon/10059.png", 70, Type.FIGHTING, Type.STEEL),
+    MEGA_ABOMASNOW("Mega Abomasnow", "460", "src/main/resources/pokemon/10060.png", 90, Type.GRASS, Type.ICE),
+    MEGA_LATIAS("Mega Latias", "380", "src/main/resources/pokemon/10062.png", 80, Type.DRAGON, Type.PSYCHIC),
+    MEGA_LATIOS("Mega Latios", "381", "src/main/resources/pokemon/10063.png", 80, Type.DRAGON, Type.PSYCHIC),
+    MEGA_SWAMPERT("Mega Swampert", "260", "src/main/resources/pokemon/10064.png", 100, Type.WATER, Type.GROUND),
+    MEGA_SCEPTILE("Mega Sceptile", "254", "src/main/resources/pokemon/10065.png", 70, Type.GRASS),
+    MEGA_SABLEYE("Mega Sableye", "302", "src/main/resources/pokemon/10066.png", 50, Type.DARK, Type.GHOST),
+    MEGA_ALTARIA("Mega Altaria", "334", "src/main/resources/pokemon/10067.png", 75, Type.DRAGON, Type.FAIRY),
+    MEGA_GALLADE("Mega Gallade", "475", "src/main/resources/pokemon/10068.png", 68, Type.PSYCHIC, Type.FIGHTING),
+    MEGA_AUDINO("Mega Audino", "531", "src/main/resources/pokemon/10069.png", 103, Type.NORMAL),
+    MEGA_SHARPEDO("Mega Sharpedo", "319", "src/main/resources/pokemon/10070.png", 70, Type.WATER, Type.DARK),
+    MEGA_SLOWBRO("Mega Slowbro", "080", "src/main/resources/pokemon/10071.png", 95, Type.WATER, Type.PSYCHIC),
+    MEGA_STEELIX("Mega Steelix", "208", "src/main/resources/pokemon/10072.png", 75, Type.STEEL, Type.GROUND),
+    MEGA_PIDGEOT("Mega Pidgeot", "018", "src/main/resources/pokemon/10073.png", 83, Type.NORMAL, Type.FLYING),
+    MEGA_GLALIE("Mega Glalie", "362", "src/main/resources/pokemon/10074.png", 80, Type.ICE),
+    MEGA_DIANCIE("Mega Diancie", "719", "src/main/resources/pokemon/10075.png", 50, Type.ROCK, Type.FAIRY),
+    MEGA_METAGROSS("Mega Metagross", "376", "src/main/resources/pokemon/10076.png", 80, Type.STEEL, Type.PSYCHIC),
+    PRIMAL_KYOGRE("Primal Kyogre", "382", "src/main/resources/pokemon/10077.png", 100, Type.WATER),
+    PRIMAL_GROUDON("Primal Groudon", "383", "src/main/resources/pokemon/10078.png", 100, Type.GROUND),
+    MEGA_RAYQUAZA("Mega Rayquaza", "384", "src/main/resources/pokemon/10079.png", 105, Type.DRAGON, Type.FLYING),
+    HOOPA_UNBOUND("Hoopa (Unbound)", "720", "src/main/resources/pokemon/10086.png", 80, Type.PSYCHIC, Type.GHOST),
+    MEGA_CAMERUPT("Mega Camerupt", "323", "src/main/resources/pokemon/10087.png", 70, Type.FIRE, Type.GROUND),
+    MEGA_LOPUNNY("Mega Lopunny", "428", "src/main/resources/pokemon/10088.png", 65, Type.NORMAL),
+    MEGA_SALAMENCE("Mega Salamence", "373", "src/main/resources/pokemon/10089.png", 95, Type.DRAGON, Type.FLYING),
+    MEGA_BEEDRILL("Mega Beedrill", "015", "src/main/resources/pokemon/10090.png", 65, Type.BUG, Type.POISON);
+
+
+    private final int HP;
+    private final Type[] TYPE;
+    private final String NAME, NAT_DEX_NUMBER;
+    private ImageIcon IMG;
+
+    Generation6(final String name, final String dexNumber, final String path, final int hp, final Type... type) {
+        HP = hp;
+        NAT_DEX_NUMBER = dexNumber;
+        NAME = name;
+        TYPE = type;
+        IMG = new ImageIcon(new ImageIcon(path).getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH));
+
+    }
+
+    public String getDexNumber()
+    {
+        return NAT_DEX_NUMBER;
+    }
+
+    public int getHP(){
+        return HP;
+    }
+
+    /**
+     * Returns Name of the Pokemon
+     *
+     * @return Name of the Pokemon
+     */
+    public String getName()
+    {
+        return NAME;
+    }
+
+    /**
+     * Gets the first type of the Pokemon
+     * @return Their first type
+     */
+    public Type[] getType()
+    {
+        return TYPE;
+    }
+
+    public ImageIcon getImg(){
+        return IMG;
+    }
+}
---
+++ src/main/java/org/pokemon/Generation7.java
@@ -0,0 +1,176 @@
+package org.pokemon;
+
+import jdk.dynalink.linker.support.TypeUtilities;
+
+import javax.swing.*;
+import java.awt.*;
+
+public enum Generation7 {
+    ROWLET("Rowlet", "722", "src/main/resources/pokemon/722.png", 68, Type.GRASS, Type.FLYING),
+    DARTRIX("Dartrix", "723", "src/main/resources/pokemon/723.png", 78, Type.GRASS, Type.FLYING),
+    DECIDUEYE("Decidueye", "724", "src/main/resources/pokemon/724.png", 78, Type.GRASS, Type.GHOST),
+    LITTEN("Litten", "725", "src/main/resources/pokemon/725.png", 45, Type.FIRE),
+    TORRACAT("Torracat", "726", "src/main/resources/pokemon/726.png", 65, Type.FIRE),
+    INCINEROAR("Incineroar", "727", "src/main/resources/pokemon/727.png", 95, Type.FIRE, Type.DARK),
+    POPPLIO("Popplio", "728", "src/main/resources/pokemon/728.png", 50, Type.WATER),
+    BRIONNE("Brionne", "729", "src/main/resources/pokemon/729.png", 60, Type.WATER),
+    PRIMARINA("Primarina", "730", "src/main/resources/pokemon/730.png", 80, Type.WATER, Type.FAIRY),
+    PIKIPEK("Pikipek", "731", "src/main/resources/pokemon/731.png", 35, Type.NORMAL, Type.FLYING),
+    TRUMBEAK("Trumbeak", "732", "src/main/resources/pokemon/732.png", 55, Type.NORMAL, Type.FLYING),
+    TOUCANNON("Toucannon", "733", "src/main/resources/pokemon/733.png", 80, Type.NORMAL, Type.FLYING),
+    YUNGOOS("Yungoos", "734", "src/main/resources/pokemon/734.png", 48, Type.NORMAL),
+    GUMSHOOS("Gumshoos", "735", "src/main/resources/pokemon/735.png", 88, Type.NORMAL),
+    GRUBBIN("Grubbin", "736", "src/main/resources/pokemon/736.png", 47, Type.BUG),
+    CHARJABUG("Charjabug", "737", "src/main/resources/pokemon/737.png", 57, Type.BUG, Type.ELECTRIC),
+    VIKAVOLT("Vikavolt", "738", "src/main/resources/pokemon/738.png", 77, Type.BUG, Type.ELECTRIC),
+    CRABRAWLER("Crabrawler", "739", "src/main/resources/pokemon/739.png", 47, Type.FIGHTING),
+    CRABOMINABLE("Crabominable", "740", "src/main/resources/pokemon/740.png", 97, Type.FIGHTING, Type.ICE),
+    ORICORIO_BAILE("Oricorio (Baile Style)", "741", "src/main/resources/pokemon/741.png", 75, Type.FIRE, Type.FLYING),
+    CUTIEFLY("Cutiefly", "742", "src/main/resources/pokemon/742.png", 40, Type.BUG, Type.FAIRY),
+    RIBOMBEE("Ribombee", "743", "src/main/resources/pokemon/743.png", 60, Type.BUG, Type.FAIRY),
+    ROCKRUFF("Rockruff", "744", "src/main/resources/pokemon/744.png", 45, Type.ROCK),
+    LYCANROC_MIDDAY("Lycanroc (Midday Form)", "745", "src/main/resources/pokemon/745.png", 75, Type.ROCK),
+    WISHIWASHI_SOLO("Wishiwashi (Solo Form)", "746", "src/main/resources/pokemon/746.png", 45, Type.WATER),
+    MAREANIE("Mareanie", "747", "src/main/resources/pokemon/747.png", 50, Type.POISON, Type.WATER),
+    TOXAPEX("Toxapex", "748", "src/main/resources/pokemon/748.png", 50, Type.POISON, Type.WATER),
+    MUDBRAY("Mudbray", "749", "src/main/resources/pokemon/749.png", 70, Type.GROUND),
+    MUDSDALE("Mudsdale", "750", "src/main/resources/pokemon/750.png", 100, Type.GROUND),
+    DEWPIDER("Dewpider", "751", "src/main/resources/pokemon/751.png", 38, Type.WATER, Type.BUG),
+    ARAQUANID("Araquanid", "752", "src/main/resources/pokemon/752.png", 68, Type.WATER, Type.BUG),
+    FOMANTIS("Fomantis", "753", "src/main/resources/pokemon/753.png", 40, Type.GRASS),
+    LURANTIS("Lurantis", "754", "src/main/resources/pokemon/754.png", 70, Type.GRASS),
+    MORELULL("Morelull", "755", "src/main/resources/pokemon/755.png", 40, Type.GRASS, Type.FAIRY),
+    SHIINOTIC("Shiinotic", "756", "src/main/resources/pokemon/756.png", 60, Type.GRASS, Type.FAIRY),
+    SALANDIT("Salandit", "757", "src/main/resources/pokemon/757.png", 48, Type.POISON, Type.FIRE),
+    SALAZZLE("Salazzle", "758", "src/main/resources/pokemon/758.png", 68, Type.POISON, Type.FIRE),
+    STUFFUL("Stufful", "759", "src/main/resources/pokemon/759.png", 70, Type.NORMAL, Type.FIGHTING),
+    BEWEAR("Bewear", "760", "src/main/resources/pokemon/760.png", 120, Type.NORMAL, Type.FIGHTING),
+    BOUNSWEET("Bounsweet", "761", "src/main/resources/pokemon/761.png", 42, Type.GRASS),
+    STEENEE("Steenee", "762", "src/main/resources/pokemon/762.png", 52, Type.GRASS),
+    TSAREENA("Tsareena", "763", "src/main/resources/pokemon/763.png", 72, Type.GRASS),
+    COMFEY("Comfey", "764", "src/main/resources/pokemon/764.png", 51, Type.FAIRY),
+    ORANGURU("Oranguru", "765", "src/main/resources/pokemon/765.png", 90, Type.NORMAL, Type.PSYCHIC),
+    PASSIMIAN("Passimian", "766", "src/main/resources/pokemon/766.png", 100, Type.FIGHTING),
+    WIMPOD("Wimpod", "767", "src/main/resources/pokemon/767.png", 25, Type.BUG, Type.WATER),
+    GOLISOPOD("Golisopod", "768", "src/main/resources/pokemon/768.png", 75, Type.BUG, Type.WATER),
+    SANDYGAST("Sandygast", "769", "src/main/resources/pokemon/769.png", 55, Type.GHOST, Type.GROUND),
+    PALOSSAND("Palossand", "770", "src/main/resources/pokemon/770.png", 85, Type.GHOST, Type.GROUND),
+    PYUKUMUKU("Pyukumuku", "771", "src/main/resources/pokemon/771.png", 55, Type.WATER),
+    TYPE_NULL("Type: Null", "772", "src/main/resources/pokemon/772.png", 95, Type.NORMAL),
+    SILVALLY("Silvally", "773", "src/main/resources/pokemon/773.png", 95, Type.NORMAL),
+    MINIOR("Minior", "774", "src/main/resources/pokemon/774.png", 60, Type.ROCK, Type.FLYING),
+    KOMALA("Komala", "775", "src/main/resources/pokemon/775.png", 65, Type.NORMAL),
+    TURTONATOR("Turtonator", "776", "src/main/resources/pokemon/776.png", 60, Type.FIRE, Type.DRAGON),
+    TOGEDEMARU("Togedemaru", "777", "src/main/resources/pokemon/777.png", 65, Type.ELECTRIC, Type.STEEL),
+    MIMIKYU("Mimikyu", "778", "src/main/resources/pokemon/778.png", 55, Type.GHOST, Type.FAIRY),
+    BRUXISH("Bruxish", "779", "src/main/resources/pokemon/779.png", 68, Type.WATER, Type.PSYCHIC),
+    DRAMPA("Drampa", "780", "src/main/resources/pokemon/780.png", 78, Type.NORMAL, Type.DRAGON),
+    DHELMISE("Dhelmise", "781", "src/main/resources/pokemon/781.png", 70, Type.GHOST, Type.GRASS),
+    JANGMO_O("Jangmo-o", "782", "src/main/resources/pokemon/782.png", 45, Type.DRAGON),
+    HAKAMO_O("Hakamo-o", "783", "src/main/resources/pokemon/783.png", 55, Type.DRAGON, Type.FIGHTING),
+    KOMMO_O("Kommo-o", "784", "src/main/resources/pokemon/784.png", 75, Type.DRAGON, Type.FIGHTING),
+    TAPU_KOKO("Tapu Koko", "785", "src/main/resources/pokemon/785.png", 70, Type.ELECTRIC, Type.FAIRY),
+    TAPU_LELE("Tapu Lele", "786", "src/main/resources/pokemon/786.png", 70, Type.PSYCHIC, Type.FAIRY),
+    TAPU_BULU("Tapu Bulu", "787", "src/main/resources/pokemon/787.png", 70, Type.GRASS, Type.FAIRY),
+    TAPU_FINI("Tapu Fini", "788", "src/main/resources/pokemon/788.png", 70, Type.WATER, Type.FAIRY),
+    COSMOG("Cosmog", "789", "src/main/resources/pokemon/789.png", 43, Type.PSYCHIC),
+    COSMOEM("Cosmoem", "790", "src/main/resources/pokemon/790.png", 43, Type.PSYCHIC),
+    SOLGALEO("Solgaleo", "791", "src/main/resources/pokemon/791.png", 137, Type.PSYCHIC, Type.STEEL),
+    LUNALA("Lunala", "792", "src/main/resources/pokemon/792.png", 137, Type.PSYCHIC, Type.GHOST),
+    NIHILEGO("Nihilego", "793", "src/main/resources/pokemon/793.png", 109, Type.ROCK, Type.POISON),
+    BUZZWOLE("Buzzwole", "794", "src/main/resources/pokemon/794.png", 107, Type.BUG, Type.FIGHTING),
+    PHEROMOSA("Pheromosa", "795", "src/main/resources/pokemon/795.png", 71, Type.BUG, Type.FIGHTING),
+    XURKITREE("Xurkitree", "796", "src/main/resources/pokemon/796.png", 83, Type.ELECTRIC),
+    CELESTEELA("Celesteela", "797", "src/main/resources/pokemon/797.png", 97, Type.STEEL, Type.FLYING),
+    KARTANA("Kartana", "798", "src/main/resources/pokemon/798.png", 59, Type.GRASS, Type.STEEL),
+    GUZZLORD("Guzzlord", "799", "src/main/resources/pokemon/799.png", 223, Type.DARK, Type.DRAGON),
+    NECROZMA("Necrozma", "800", "src/main/resources/pokemon/800.png", 97, Type.PSYCHIC),
+    MAGEARNA("Magearna", "801", "src/main/resources/pokemon/801.png", 80, Type.STEEL, Type.FAIRY),
+    MARSHADOW("Marshadow", "802", "src/main/resources/pokemon/802.png", 90, Type.FIGHTING, Type.GHOST),
+    POIPOLE("Poipole", "803", "src/main/resources/pokemon/803.png", 67, Type.POISON),
+    NAGANADEL("Naganadel", "804", "src/main/resources/pokemon/804.png", 73, Type.POISON, Type.DRAGON),
+    STAKATAKA("Stakataka", "805", "src/main/resources/pokemon/805.png", 61, Type.ROCK, Type.STEEL),
+    BLACEPHALON("Blacephalon", "806", "src/main/resources/pokemon/806.png", 53, Type.FIRE, Type.GHOST),
+    ZERAORA("Zeraora", "807", "src/main/resources/pokemon/807.png", 88, Type.ELECTRIC),
+    MELTAN("Meltan", "808", "src/main/resources/pokemon/808.png", 46, Type.STEEL),
+    MELMETAL("Melmetal", "809", "src/main/resources/pokemon/809.png", 135, Type.STEEL),
+
+    ALOLAN_RATTATA("Alolan Rattata", "019", "src/main/resources/pokemon/10091.png", 30, Type.NORMAL, Type.DARK),
+    ALOLAN_RATICATE("Alolan Raticate", "020", "src/main/resources/pokemon/10092.png", 75, Type.NORMAL, Type.DARK),
+    ALOLAN_RAICHU("Alolan Raichu", "026", "src/main/resources/pokemon/10100.png", 60, Type.ELECTRIC, Type.PSYCHIC),
+    ALOLAN_SANDSHREW("Alolan Sandshrew", "027", "src/main/resources/pokemon/10101.png", 50, Type.ICE, Type.STEEL),
+    ALOLAN_SANDSLASH("Alolan Sandslash", "028", "src/main/resources/pokemon/10102.png", 75, Type.ICE, Type.STEEL),
+    ALOLAN_VULPIX("Alolan Vulpix", "037", "src/main/resources/pokemon/0103.png", 38, Type.ICE, Type.FAIRY),
+    ALOLAN_NINETALES("Alolan Ninetales", "038", "src/main/resources/pokemon/10104.png", 73, Type.ICE, Type.FAIRY),
+    ALOLAN_DIGLETT("Alolan Diglett", "050", "src/main/resources/pokemon/10105.png", 10, Type.GROUND, Type.STEEL),
+    ALOLAN_DUGTRIO("Alolan Dugtrio", "051", "src/main/resources/pokemon/10106.png", 35, Type.GROUND, Type.STEEL),
+    ALOLAN_MEOWTH("Alolan Meowth", "052", "src/main/resources/pokemon/10107.png", 40, Type.DARK),
+    ALOLAN_PERSIAN("Alolan Persian", "053", "src/main/resources/pokemon/10108.png", 75, Type.DARK),
+    ALOLAN_GEODUDE("Alolan Geodude", "074", "src/main/resources/pokemon/10109.png", 40, Type.ROCK, Type.ELECTRIC),
+    ALOLAN_GRAVELER("Alolan Graveler", "075", "src/main/resources/pokemon/10110.png", 55, Type.ROCK, Type.ELECTRIC),
+    ALOLAN_GOLEM("Alolan Golem", "076", "src/main/resources/pokemon/10111.png", 80, Type.ROCK, Type.ELECTRIC),
+    ALOLAN_GRIMER("Alolan Grimer", "088", "src/main/resources/pokemon/10112.png", 80, Type.POISON, Type.DARK),
+    ALOLAN_MUK("Alolan Muk", "089", "src/main/resources/pokemon/10113.png", 105, Type.POISON, Type.DARK),
+    ALOLAN_EXEGGUTOR("Alolan Exeggutor", "103", "src/main/resources/pokemon/10114.png", 95, Type.GRASS, Type.DRAGON),
+    ALOLAN_MAROWAK("Alolan Marowak", "105", "src/main/resources/pokemon/10115.png", 60, Type.FIRE, Type.GHOST),
+    ASH_GRENINJA("Ash's Greninja", "658", "src/main/resources/pokemon/10116.png", 72, Type.WATER, Type.DARK),
+    ZYGARDE_10("Zygarde (10% Forme)", "718", "src/main/resources/pokemon/10118.png", 54, Type.DRAGON, Type.GROUND),
+    ZYGARDE_100("Zygarde (Complete Forme)", "718", "src/main/resources/pokemon/10120.png", 216, Type.DRAGON, Type.GROUND),
+    ORICORIO_POMPOM("Oricorio (Pom Pom Style)", "741", "src/main/resources/pokemon/10123.png", 75, Type.ELECTRIC, Type.FLYING),
+    ORICORIO_PAU("Oricorio (Pa'u Style)", "741", "src/main/resources/pokemon/10124.png", 75, Type.PSYCHIC, Type.FLYING),
+    ORICORIO_SENSU("Oricorio (Sensu Style)", "741", "src/main/resources/pokemon/10125.png", 75, Type.GHOST, Type.FLYING),
+    LYCANROC_MIDNIGHT("Lycanroc (Midnight Form)", "745", "src/main/resources/pokemon/10126.png", 85, Type.ROCK),
+    WISHIWASHI_SCHOOL("Wishiwashi (School Form)", "746", "src/main/resources/pokemon/10127.png", 45, Type.WATER),
+    MINIOR_CORE("Minior (Core form)", "774", "src/main/resources/pokemon/10136.png", 60, Type.ROCK, Type.FLYING),
+    LYCANROC_CREPUSCULAR("Lycanroc (Crepuscular Form)", "745", "src/main/resources/pokemon/10152.png", 75, Type.ROCK),
+    DUSK_MANE_NECROZMA("Dusk Mane Necrozma", "800", "src/main/resources/pokemon/10155.png", 97, Type.PSYCHIC,Type.STEEL),
+    DAWN_WINGS_NECROZMA("Dawn Wings Necrozma", "800", "src/main/resources/pokemon/10156.png", 97, Type.PSYCHIC, Type.GHOST),
+    ULTRA_NECROZMA("Ultra Necrozma", "800", "src/main/resources/pokemon/10157.png", 97, Type.PSYCHIC, Type.DRAGON);
+
+
+    private final int HP;
+    private final Type[] TYPE;
+    private final String NAME, NAT_DEX_NUMBER;
+    private ImageIcon IMG;
+
+    Generation7(final String name, final String dexNumber, final String path, final int hp, final Type... type) {
+        HP = hp;
+        NAT_DEX_NUMBER = dexNumber;
+        NAME = name;
+        TYPE = type;
+        IMG = new ImageIcon(new ImageIcon(path).getImage().getScaledInstance(400, 400, Image.SCALE_SMOOTH));
+
+    }
+
+    public String getDexNumber()
+    {
+        return NAT_DEX_NUMBER;
+    }
+
+    public int getHP(){
+        return HP;
+    }
+
+    /**
+     * Returns Name of the Pokemon
+     *
+     * @return Name of the Pokemon
+     */
+    public String getName()
+    {
+        return NAME;
+    }
+
+    /**
+     * Gets the first type of the Pokemon
+     * @return Their first type
+     */
+    public Type[] getType()
+    {
+        return TYPE;
+    }
+
+    public ImageIcon getImg(){
+        return IMG;
+    }
+}
